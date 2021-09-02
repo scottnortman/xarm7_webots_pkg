a2e4c4d038b40f728a01841b8b23c7b574e8fe4a
Scott Nortman <scott.nortman@gmail.com>
2021-09-01 23:39:02 -0400

    Trying to get cartesian motion working

--- launch/xarm7_webots_moveit.launch.py
+++ launch/xarm7_webots_moveit.launch.py
@@ -6,6 +6,125 @@ Date:   Aug 30 2021
 Auth:   scott@restfulrobotics.com
 Note:
 
+Example
+
+$ cd ~/ros2_ws
+$ ros2 launch src/xarm7_webots_pkg/launch/xarm7_webots_moveit.launch.py 
+
+
+Additional notes
+
+https://github.com/RIF-Robotics/ar3_ros/blob/main/ar3_moveit_config/launch/direct_joint_control.launch.py
+
+
+
+ros2 service call controller_manager/load_and_start_controller controller_manager_msgs/srv/LoadStartController \'{name: "joint_state_controller"}\'
+
+
+
+ros2 service call controller_manager/load_and_start_controller controller_manager_msgs/srv/LoadStartController '{name: "xarm_gripper_traj_controller"}'
+
+To see what controllers are available
+
+$ ros2 service call /controller_manager/list_controllers controller_manager_msgs/srv/ListControllers
+
+response:
+controller_manager_msgs.srv.ListControllers_Response
+(
+    controller=
+    [
+        controller_manager_msgs.msg.ControllerState(
+            name='xarm_gripper_traj_controller', 
+            state='active', 
+            type='joint_trajectory_controller/JointTrajectoryController', 
+            claimed_interfaces=
+            [
+                'drive_joint/position'
+            ]
+        ), 
+        controller_manager_msgs.msg.ControllerState(
+            name='xarm7_traj_controller', 
+            state='active', 
+            type='joint_trajectory_controller/JointTrajectoryController', 
+            claimed_interfaces=
+            [
+                'joint1/position', 
+                'joint1/velocity', 
+                'joint2/position', 
+                'joint2/velocity', 
+                'joint3/position', 
+                'joint3/velocity', 
+                'joint4/position', 
+                'joint4/velocity', 
+                'joint5/position', 
+                'joint5/velocity', 
+                'joint6/position', 
+                'joint6/velocity', 
+                'joint7/position', 
+                'joint7/velocity'
+            ]
+        )
+    ]
+)
+
+$ ros2 service call /controller_manager/list_controller_types controller_manager_msgs/srv/ListControllerTypes 
+waiting for service to become available...
+requester: making request: controller_manager_msgs.srv.ListControllerTypes_Request()
+
+response:
+controller_manager_msgs.srv.ListControllerTypes_Response
+(
+    types=
+    [
+        'controller_manager/test_controller', 
+        'controller_manager/test_controller_failed_init', 
+        'controller_manager/test_controller_with_interfaces', 
+        'diff_drive_controller/DiffDriveController', 
+        'effort_controllers/GripperActionController', 
+        'effort_controllers/JointGroupEffortController', 
+        'forward_command_controller/ForwardCommandController', 
+        'joint_state_broadcaster/JointStateBroadcaster', 
+        'joint_state_controller/JointStateController', 
+        'joint_trajectory_controller/JointTrajectoryController', 
+        'position_controllers/GripperActionController', 
+        'position_controllers/JointGroupPositionController', 
+        'velocity_controllers/JointGroupVelocityController'
+    ], 
+    base_classes=
+    [
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface', 
+        'controller_interface::ControllerInterface'
+    ]
+)
+
+# http://docs.ros.org/en/api/controller_manager_msgs/html/srv/SwitchController.html
+$ ros2 service call /controller_manager/switch_controller controller_manager_msgs/srv/SwitchController \
+    "{ \
+        start_controllers: ['position_controllers/JointGroupPositionController'], \
+        stop_controllers: ['joint_trajectory_controller/JointTrajectoryController'],\
+        strictness: 0, \
+        start_asap: False, \
+    }"
+
+
+
+IK python code
+https://leechangyo.github.io/ros/2019/10/05/ROS-TUTORIAL5/
+
+
+
+
 
 '''
 
@@ -62,8 +181,8 @@ def generate_launch_description():
 
 
     ld.add_action(static_frame_world_tx_link_base)
-    ld.add_action(webots_node)
+    #ld.add_action(webots_node)
     ld.add_action(xarm_planner_node)
-    ld.add_action(xarm_traj_req_node)
+    #ld.add_action(xarm_traj_req_node)
 
     return ld
